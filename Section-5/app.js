d514d7ad3af4dd7400fe7119268f19e1a8e04cc9
Eureka <narike@gmail.com>
2025-08-05 16:23:01 +0300

    end of section 6

--- Section-5/app.js
+++ Section-5/app.js
@@ -1,20 +1,24 @@
-const bodyParser = require('body-parser');
-const express = require('express')
 const path = require('path');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use('/admin', adminRoutes);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use('/', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '' });
+});
 
 app.listen(3000);
