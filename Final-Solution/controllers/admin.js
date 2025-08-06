c6276052b4890d7c77d157fe73c46d0505f895e2
Eureka <narike@gmail.com>
2025-08-06 17:37:24 +0300

    end of section 9 (dynamic routing)

---
+++ Final-Solution/controllers/admin.js
@@ -0,0 +1,69 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/edit-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    editing: false
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  console.log(productId);
+  const editMode = req.query.edit;
+  Product.findById(productId, product => {
+    if (!product) {
+      console.log('no product')
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
+};
+
+exports.postEditProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const id = req.body.id;
+
+  const newProduct = new Product(id, title, imageUrl, description, price);
+
+  newProduct.save();
+
+  res.redirect('/admin/products')
+
+};
+
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('admin/products', {
+      prods: products,
+      pageTitle: 'Admin Products',
+      path: '/admin/products'
+    });
+  });
+};
+
+exports.deleteProduct = (req, res, next) => {
+  const id = req.body.id;
+  Product.deleteProduct(id)
+  return res.redirect('/admin/products')
+}
---
+++ Final-Solution/models/cart.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err && fileContent.length > 0) {
+        cart = JSON.parse(fileContent);
+      }
+
+      const existingProduct = cart.products.find(prod => prod.id === id);
+
+      if (existingProduct) {
+        existingProduct.qty += 1;
+      } else {
+        cart.products.push({ id: id, qty: 1 });
+      }
+
+      cart.totalPrice = Number(cart.totalPrice) + Number(productPrice);
+
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        if (err) console.log(err);
+      });
+    });
+  }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id)
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(p => p.id !== id)
+      updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      })
+    })
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
+  }
+};
